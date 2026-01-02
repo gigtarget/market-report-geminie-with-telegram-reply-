2cbf02fff45c4071799c7f03f6a318e43c6e4ec0
gigtarget <jatingots9@gmail.com>
2026-01-02 00:51:31 -0500

    Handle invalid telegram token gracefully

--- src/server.ts
+++ src/server.ts
@@ -28,7 +28,7 @@ app.register(generateRoute);
 
 const start = async () => {
   try {
-    initBot();
+    await initBot();
     await app.listen({ port: config.port, host: '0.0.0.0' });
   } catch (err) {
     app.log.error(err);
